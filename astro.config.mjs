4418337f16efa0d48e6aca7c50262a6944a19356
Floris Fiedeldij Dop <mrfloris@gmail.com>
2026-05-29 02:12:31 +0200

    Add Minecraft-inspired docs theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeFlexoki from 'starlight-theme-flexoki';
 
 export default defineConfig({
   site: 'https://docs.1moreblock.com',
@@ -17,6 +18,7 @@ export default defineConfig({
           href: 'https://github.com/mrfdev/1MB-Plugins-Docs',
         },
       ],
+      plugins: [starlightThemeFlexoki({ accentColor: 'green' })],
       customCss: ['./src/styles/custom.css'],
       sidebar: [
         { label: 'Home', link: '/' },
